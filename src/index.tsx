a2fc59dc61b8d9257c5bc4c308bb0d261646f485
robin <mgh3326@naver.com>
2021-01-27 22:08:58 +0900

    15.15 Using the Token

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloProvider } from "@apollo/client";
+import {ApolloProvider} from "@apollo/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {HelmetProvider} from "react-helmet-async";
+
 import "./styles/styles.css";
-import { client } from "./apollo";
+import {client} from "./apollo";
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
