fb013eeadb0d509bc3b0c7e54c1f74a64021a603
robin <mgh3326@naver.com>
2021-01-27 21:08:30 +0900

    15.12 Create Account Mutation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,15 @@
-import {gql, useMutation} from "@apollo/client";
+import { gql, useMutation } from "@apollo/client";
 import React from "react";
-import {useForm} from "react-hook-form";
-import {Link} from "react-router-dom";
-import {Button} from "../components/button";
-import {FormError} from "../components/form-error";
+import Helmet from "react-helmet";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { Button } from "../components/button";
+import { FormError } from "../components/form-error";
 import nuberLogo from "../images/logo.svg";
-import {loginMutation, loginMutationVariables,} from "../__generated__/loginMutation";
+import {
+  loginMutation,
+  loginMutationVariables,
+} from "../__generated__/loginMutation";
 
 const LOGIN_MUTATION = gql`
     mutation loginMutation($loginInput: LoginInput!) {
@@ -34,19 +38,21 @@ export const Login = () => {
   });
   const onCompleted = (data: loginMutation) => {
     const {
-      login: {ok, token},
+      login: { ok, token },
     } = data;
     if (ok) {
       console.log(token);
     }
   };
-  const [loginMutation, {data: loginMutationResult, loading}] = useMutation<loginMutation,
-    loginMutationVariables>(LOGIN_MUTATION, {
+  const [loginMutation, { data: loginMutationResult, loading }] = useMutation<
+    loginMutation,
+    loginMutationVariables
+    >(LOGIN_MUTATION, {
     onCompleted,
   });
   const onSubmit = () => {
     if (!loading) {
-      const {email, password} = getValues();
+      const { email, password } = getValues();
       loginMutation({
         variables: {
           loginInput: {
@@ -59,6 +65,9 @@ export const Login = () => {
   };
   return (
     <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <Helmet>
+        <title>Login | Nuber Eats</title>
+      </Helmet>
       <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
         <img src={nuberLogo} className="w-52 mb-10" />
         <h4 className="w-full font-medium text-left text-3xl mb-5">
@@ -69,7 +78,7 @@ export const Login = () => {
           className="grid gap-3 mt-5 w-full mb-5"
         >
           <input
-            ref={register({required: "Email is required"})}
+            ref={register({ required: "Email is required" })}
             name="email"
             required
             type="email"
@@ -80,7 +89,7 @@ export const Login = () => {
             <FormError errorMessage={errors.email?.message} />
           )}
           <input
-            ref={register({required: "Password is required"})}
+            ref={register({ required: "Password is required" })}
             required
             name="password"
             type="password"
