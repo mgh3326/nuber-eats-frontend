79fac366a136128970ea921235dd831ca899d194
robin <mgh3326@naver.com>
2021-01-28 23:55:27 +0900

    17.0 Restaurants Query

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,12 @@
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Link } from "react-router-dom";
-import { useMe } from "../hooks/useMe";
+import {Link} from "react-router-dom";
+import {useMe} from "../hooks/useMe";
 import nuberLogo from "../images/logo.svg";
 
 export const Header: React.FC = () => {
-  const { data } = useMe();
+  const {data} = useMe();
   return (
     <>
       {!data?.me.verified && (
@@ -17,11 +17,11 @@ export const Header: React.FC = () => {
       <header className="py-4">
         <div className="w-full px-5 xl:px-0 max-w-screen-2xl mx-auto flex justify-between items-center">
           <Link to="/">
-            <img src={nuberLogo} className="w-36" alt="Nuber Eats" />
+            <img src={nuberLogo} className="w-44" alt="Nuber Eats" />
           </Link>
           <span className="text-xs">
             <Link to="/edit-profile">
-              <FontAwesomeIcon icon={faUser} className="text-xl" />
+              <FontAwesomeIcon icon={faUser} className="text-3xl" />
             </Link>
           </span>
         </div>
--- src/pages/user/confirm-email.tsx
+++ src/pages/user/confirm-email.tsx
@@ -1,11 +1,9 @@
-import { gql, useApolloClient, useMutation } from "@apollo/client";
-import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { useMe } from "../../hooks/useMe";
-import {
-  verifyEmail,
-  verifyEmailVariables,
-} from "../../__generated__/verifyEmail";
+import {gql, useApolloClient, useMutation} from "@apollo/client";
+import React, {useEffect} from "react";
+import {Helmet} from "react-helmet-async";
+import {useHistory} from "react-router-dom";
+import {useMe} from "../../hooks/useMe";
+import {verifyEmail, verifyEmailVariables,} from "../../__generated__/verifyEmail";
 
 const VERIFY_EMAIL_MUTATION = gql`
     mutation verifyEmail($input: VerifyEmailInput!) {
@@ -17,12 +15,12 @@ const VERIFY_EMAIL_MUTATION = gql`
 `;
 
 export const ConfirmEmail = () => {
-  const { data: userData } = useMe();
+  const {data: userData} = useMe();
   const client = useApolloClient();
   const history = useHistory();
   const onCompleted = (data: verifyEmail) => {
     const {
-      verifyEmail: { ok },
+      verifyEmail: {ok},
     } = data;
     if (ok && userData?.me.id) {
       client.writeFragment({
@@ -57,6 +55,9 @@ export const ConfirmEmail = () => {
   }, [verifyEmail]);
   return (
     <div className="mt-52 flex flex-col items-center justify-center">
+      <Helmet>
+        <title>Verify Email | Nuber Eats</title>
+      </Helmet>
       <h2 className="text-lg mb-1 font-medium">Confirming email...</h2>
       <h4 className="text-gray-700 text-sm">
         Please wait, don't close this page...
