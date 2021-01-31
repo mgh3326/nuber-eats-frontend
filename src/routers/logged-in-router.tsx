a23805489f351447f5a7f99e31b2ef9f0a53f6bf
robin <mgh3326@naver.com>
2021-01-31 09:01:32 +0900

    17.5 Search part Two

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,30 +1,30 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
-import { Header } from "../components/header";
-import { useMe } from "../hooks/useMe";
-import { Restaurants } from "../pages/client/restaurants";
-import { ConfirmEmail } from "../pages/user/confirm-email";
-import { EditProfile } from "../pages/user/edit-profile";
+import {BrowserRouter as Router, Route, Switch,} from "react-router-dom";
+import {Header} from "../components/header";
+import {useMe} from "../hooks/useMe";
+import {NotFound} from "../pages/404";
+import {Restaurants} from "../pages/client/restaurants";
+import {Search} from "../pages/client/search";
+import {ConfirmEmail} from "../pages/user/confirm-email";
+import {EditProfile} from "../pages/user/edit-profile";
 
 const ClientRoutes = [
   <Route key={1} path="/" exact>
     <Restaurants />
   </Route>,
-  <Route key={2} path="/confirm" exact>
+  <Route key={2} path="/confirm">
     <ConfirmEmail />
   </Route>,
-  <Route key={3} path="/edit-profile" exact>
+  <Route key={3} path="/edit-profile">
     <EditProfile />
+  </Route>,
+  <Route key={4} path="/search">
+    <Search />
   </Route>,
 ];
 
 export const LoggedInRouter = () => {
-  const { data, loading, error } = useMe();
+  const {data, loading, error} = useMe();
   if (!data || loading || error) {
     return (
       <div className="h-screen flex justify-center items-center">
@@ -37,7 +37,9 @@ export const LoggedInRouter = () => {
       <Header />
       <Switch>
         {data.me.role === "Client" && ClientRoutes}
-        <Redirect to="/" />
+        <Route>
+          <NotFound />
+        </Route>
       </Switch>
     </Router>
   );
