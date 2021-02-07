a494bf2346e9a9eb8d1066585d059ef7a5113418
robin <mgh3326@naver.com>
2021-02-07 12:49:13 +0900

    18.9 CreateAccount Tests part Two

--- src/pages/create-account.tsx
+++ src/pages/create-account.tsx
@@ -9,7 +9,7 @@ import nuberLogo from "../images/logo.svg";
 import {createAccountMutation, createAccountMutationVariables,} from "../__generated__/createAccountMutation";
 import {UserRole} from "../__generated__/globalTypes";
 
-const CREATE_ACCOUNT_MUTATION = gql`
+export const CREATE_ACCOUNT_MUTATION = gql`
     mutation createAccountMutation($createAccountInput: CreateAccountInput!) {
         createAccount(input: $createAccountInput) {
             ok
